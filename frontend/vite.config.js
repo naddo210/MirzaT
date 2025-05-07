568adf43f46804949fc4b07946ba4dd0c908186a
Nadeem Salmani <153388875+naddo210@users.noreply.github.com>
2025-05-07 15:51:18 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://mirzat.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       }
     }
   }
