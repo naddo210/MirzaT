91cdbbb1c0ce1646df633792127b600ed3d00136
Nadeem Salmani <153388875+naddo210@users.noreply.github.com>
2025-05-08 22:58:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,13 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads', express.static('uploads'));
 
