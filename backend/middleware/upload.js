887c7f794148c2e29ab03799ed0e97281d0695f5
Nadeem Salmani <153388875+naddo210@users.noreply.github.com>
2025-05-08 18:15:44 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -35,7 +35,7 @@ const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
   limits: {
-    fileSize: 100 * 1024 * 1024 // 100MB limit
+    fileSize: 200 * 1024 * 1024 // 100MB limit
   }
 });
 
