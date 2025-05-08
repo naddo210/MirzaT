549d3ff2adefdd08d3d922eb4bd7847e0b72c40a
Nadeem Salmani <153388875+naddo210@users.noreply.github.com>
2025-05-08 22:55:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));
 
 // Routes
 app.use('/api/reviews', require('./routes/reviewRoutes'));
