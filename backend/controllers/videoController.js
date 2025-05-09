50f5f298b3e85f818ff0b8c2797232eee82df996
Nadeem Salmani <153388875+naddo210@users.noreply.github.com>
2025-05-09 12:51:42 +0530

    Update videoController.js

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -1,19 +1,45 @@
-const asyncHandler = require('express-async-handler');
-const Video = require('../models/Video');
+// const asyncHandler = require('express-async-handler');
+// const Video = require('../models/Video');
 
 
-const getVideos = asyncHandler(async (req, res) => {
-  const videos = await Video.find().sort({ createdAt: -1 });
-  res.json(videos);
-});
+// const getVideos = asyncHandler(async (req, res) => {
+//   const videos = await Video.find().sort({ createdAt: -1 });
+//   res.json(videos);
+// });
+
+// // const uploadVideo = asyncHandler(async (req, res) => {
+// //   const { title } = req.body;
+// //   const videoUrl = req.file.path;
+
+// //   const video = await Video.create({
+// //     title,
+// //     videoUrl
+// //   });
+
+// //   res.status(201).json(video);
+// // });
+
+
 
 // const uploadVideo = asyncHandler(async (req, res) => {
 //   const { title } = req.body;
-//   const videoUrl = req.file.path;
 
+
+//   if (!req.file) {
+//     res.status(400).json({ message: 'No video file uploaded' });
+//     return;
+//   }
+
+//   // Force HTTPS when creating the video URL
+//   // const videoUrl = `${req.protocol === 'http' ? 'https' : req.protocol}://${req.get('host')}/${req.file.path.replace(/\\/g, '/')}`;
+// // const videoUrl = `https://${req.get('host')}/${req.file.path.replace(/\\/g, '/')}`;
+//   const videoUrl = `https://${req.get('host')}/uploads/videos/${req.file.filename}`;
+
+
+//   // Save to database
 //   const video = await Video.create({
 //     title,
-//     videoUrl
+//     videoUrl,
 //   });
 
 //   res.status(201).json(video);
@@ -21,20 +47,40 @@ const getVideos = asyncHandler(async (req, res) => {
 
 
 
+
+// const deleteVideo = asyncHandler(async (req, res) => {
+//   const video = await Video.findByIdAndDelete(req.params.id);
+//   if (video) {
+//     res.json({ message: 'Video removed' });
+//   } else {
+//     res.status(404);
+//     throw new Error('Video not found');
+//   }
+// });
+
+// module.exports = {
+//   getVideos,
+//   uploadVideo,
+//   deleteVideo
+// };
+const asyncHandler = require('express-async-handler');
+const Video = require('../models/Video');
+
+const getVideos = asyncHandler(async (req, res) => {
+  const videos = await Video.find().sort({ createdAt: -1 });
+  res.json(videos);
+});
+
 const uploadVideo = asyncHandler(async (req, res) => {
   const { title } = req.body;
-
 
   if (!req.file) {
     res.status(400).json({ message: 'No video file uploaded' });
     return;
   }
 
-  // Force HTTPS when creating the video URL
-  // const videoUrl = `${req.protocol === 'http' ? 'https' : req.protocol}://${req.get('host')}/${req.file.path.replace(/\\/g, '/')}`;
-// const videoUrl = `https://${req.get('host')}/${req.file.path.replace(/\\/g, '/')}`;
+  // Generate the secure video URL
   const videoUrl = `https://${req.get('host')}/uploads/videos/${req.file.filename}`;
-
 
   // Save to database
   const video = await Video.create({
@@ -44,9 +90,6 @@ const uploadVideo = asyncHandler(async (req, res) => {
 
   res.status(201).json(video);
 });
-
-
-
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const video = await Video.findByIdAndDelete(req.params.id);
@@ -61,5 +104,6 @@ const deleteVideo = asyncHandler(async (req, res) => {
 module.exports = {
   getVideos,
   uploadVideo,
-  deleteVideo
+  deleteVideo,
 };
+
